9aa49b32b91ffac83f2b38a8b155a8ca50fa37dc
Hansom-wang <mrhansomwang@gmail.com>
2018-07-24 22:21:08 +0800

    Add Adapter_Pattern

---
+++ Adapter_Pattern/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter_Pattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Adapter adapter = new Adapter(new Adaptee());
+            adapter.request();
+        }
+    }
+
+    public interface Target
+    {
+         void request();
+    }
+
+    public class Adaptee
+    {
+        public void specialRequest()
+        {
+            Console.WriteLine("特殊调用");
+        }
+
+    }
+    public class Adapter : Target
+    {
+        public Adaptee adaptee;
+        public Adapter(Adaptee adaptee)
+        {
+            this.adaptee = adaptee;
+        }
+
+        public void request()
+        {
+            this.adaptee.specialRequest();
+        }
+
+    }
+
+}
